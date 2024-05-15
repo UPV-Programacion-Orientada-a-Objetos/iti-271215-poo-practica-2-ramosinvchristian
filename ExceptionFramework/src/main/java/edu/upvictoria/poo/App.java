427fe08cb7a0d1a848b8f50e8f494764ede9689c
ChristianAlberthRamosManzanares <2230321@upv.edu.mx>
2024-05-15 11:37:33 -0600

    Starting Point

--- ExceptionFramework/src/main/java/edu/upvictoria/poo/App.java
+++ ExceptionFramework/src/main/java/edu/upvictoria/poo/App.java
@@ -4,6 +4,14 @@ package edu.upvictoria.poo;
  * Hello world!
  *
  */
+
+/** Librería para la manejo de Excepciones de sentencias SQL en archivos SCV.
+Se necesita un framework de Excepciones para manejar las distintas situaciones anómalas (Excepciones) de la librería para el menejo de sentencias SQL (Práctica 1). Se deberán establecer excepciones marcadas y no marcadas dentro del framework.
+
+Del mismo modo se deberá implementar un sistema interactivo (interprete interactivo) para consultar tablas (archivos CSV) de diferentes bases de datos (carpetas). Del mismo modo se deberán agregar funciones BUIT-IN (Ver anexo).
+
+ */
+
 public class App 
 {
     public static void main( String[] args )
